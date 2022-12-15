fddbebcbb5b028fd261ea85c042e0031c6e95ea8
Krout0n <me+git@tokinia.me>
2022-12-15 19:50:48 +0900

    This commit must be failed

--- tests/device/test_psi.py
+++ tests/device/test_psi.py
@@ -26,109 +26,8 @@ class TestDevicePSI2PC(DeviceTestCase):
             }
         )
 
-        db2 = pd.DataFrame(
-            {
-                'c1': ['K3', 'K1', 'K1', 'K4'],
-                'c2': ['B3', 'A1', 'A1', 'A4'],
-                'c3': ['C3', 'C1', 'D1', 'C4'],
-                'c4': [3, 1, 9, 4],
-            }
-        )
-
-        db3 = pd.DataFrame(
-            {'c1': ['K7', 'K8', 'K9'], 'c2': ['A7', 'A8', 'A9'], 'c3': [7, 8, 9]}
-        )
-
-        db4 = pd.DataFrame(
-            {
-                'c11': ['K3', 'K1', 'K9', 'K4'],
-                'c21': ['B3', 'A1', 'A9', 'A4'],
-                'c31': [3, 1, 9, 4],
-            }
-        )
-
         self.da = sf.to(self.alice, da)
         self.db = sf.to(self.bob, db)
-        self.db2 = sf.to(self.bob, db2)
-        self.db3 = sf.to(self.bob, db3)
-        self.dc = sf.to(self.carol, db)
-        self.db4 = sf.to(self.bob, db4)
-
-    def test_single_col(self):
-
-        da, db = self.spu.psi_df('c1', [self.da, self.db], 'alice')
-
-        expected = pd.DataFrame(
-            {'c1': ['K1', 'K3', 'K4'], 'c2': ['A1', 'A3', 'A4'], 'c3': [1, 3, 4]}
-        )
-        pd.testing.assert_frame_equal(sf.reveal(da).reset_index(drop=True), expected)
-
-        expected = pd.DataFrame(
-            {'c1': ['K1', 'K3', 'K4'], 'c2': ['A1', 'B3', 'A4'], 'c3': [1, 3, 4]}
-        )
-        pd.testing.assert_frame_equal(sf.reveal(db).reset_index(drop=True), expected)
-
-    def test_multiple_col(self):
-        da, db = self.spu.psi_df(['c1', 'c2'], [self.da, self.db], 'alice')
-
-        expected = pd.DataFrame({'c1': ['K1', 'K4'], 'c2': ['A1', 'A4'], 'c3': [1, 4]})
-        pd.testing.assert_frame_equal(sf.reveal(da).reset_index(drop=True), expected)
-        pd.testing.assert_frame_equal(sf.reveal(db).reset_index(drop=True), expected)
-
-    def test_different_cols(self):
-        da, db = self.spu.psi_df(
-            {self.alice: ['c1', 'c2'], self.bob: ['c11', 'c21']},
-            [self.da, self.db4],
-            'alice',
-        )
-
-        expected_a = pd.DataFrame(
-            {'c1': ['K1', 'K4'], 'c2': ['A1', 'A4'], 'c3': [1, 4]}
-        )
-        expected_b = pd.DataFrame(
-            {'c11': ['K1', 'K4'], 'c21': ['A1', 'A4'], 'c31': [1, 4]}
-        )
-        pd.testing.assert_frame_equal(sf.reveal(da).reset_index(drop=True), expected_a)
-        pd.testing.assert_frame_equal(sf.reveal(db).reset_index(drop=True), expected_b)
-
-    def test_invalid_device(self):
-        with self.assertRaisesRegex(AssertionError, 'not co-located'):
-            da, dc = self.spu.psi_df(['c1', 'c2'], [self.da, self.dc], 'alice')
-            sf.reveal([da, dc])
-
-    def test_duplicate_col(self):
-        with self.assertRaisesRegex(RuntimeError, 'found duplicated keys'):
-            da, db = self.spu.psi_df('c1', [self.da, self.db2], 'alice')
-            sf.reveal([da, db])
-
-        # reset spu to clear corrupted state
-        self.spu.reset()
-
-    def test_missing_col(self):
-        with self.assertRaisesRegex(RuntimeError, "can't find feature names 'c4'"):
-            da, db = self.spu.psi_df(['c1', 'c4'], [self.da, self.db2], 'alice')
-            sf.reveal([da, db])
-
-        # reset spu to clear corrupted state
-        self.spu.reset()
-
-    def test_no_intersection(self):
-        da, db = self.spu.psi_df('c1', [self.da, self.db3], 'alice')
-        expected = pd.DataFrame({'c1': [], 'c2': [], 'c3': []}).astype('object')
-        pd.testing.assert_frame_equal(sf.reveal(da).reset_index(drop=True), expected)
-        pd.testing.assert_frame_equal(sf.reveal(db).reset_index(drop=True), expected)
-
-    def test_no_broadcast(self):
-        # only alice can get result
-        da, db = self.spu.psi_df(
-            'c1', [self.da, self.db], 'alice', 'KKRT_PSI_2PC', False, True, False
-        )
-        expected = pd.DataFrame(
-            {'c1': ['K1', 'K3', 'K4'], 'c2': ['A1', 'A3', 'A4'], 'c3': [1, 3, 4]}
-        )
-        pd.testing.assert_frame_equal(sf.reveal(da).reset_index(drop=True), expected)
-        # bob can not get result
-        self.assertIsNone(sf.reveal(db))
 
     def test_psi_csv(self):
         data_dir = f'.data/{uuid.uuid4()}'
@@ -146,7 +45,7 @@ class TestDevicePSI2PC(DeviceTestCase):
         sf.reveal(self.da).to_csv(input_path[self.alice], index=False)
         sf.reveal(self.db).to_csv(input_path[self.bob], index=False)
 
-        self.spu.psi_csv(['c1', 'c2'], input_path, output_path, 'alice')
+        self.spu.psi_csv(['c1'], input_path, output_path, 'alice')
 
         expected = pd.DataFrame({'c1': ['K1', 'K4'], 'c2': ['A1', 'A4'], 'c3': [1, 4]})
         da = pd.read_csv(output_path[self.alice])
